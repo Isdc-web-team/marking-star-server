e345256c72999d335e90400da7ecd99c36de7869
SujonHossain1 <sujon.hossain758@gmail.com>
2021-04-17 15:23:36 +0600

    image

---
+++ AddProduct.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const AddProduct = ({ updateProduct, isUpdate }) => {
+    const { register, handleSubmit, reset, errors } = useForm();
+    const [image, setImage1] = new FormDAte9();
+    const [product, setProduct] = useState({
+        title: '',
+        url: '',
+        category: '',
+        subCategory: '',
+        shortDescription: '',
+        stock: '',
+    });
+
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setProduct((product) => ({
+            ...product,
+            [name]: value,
+        }));
+    };
+
+    const imageHandler = (e) => {
+        const file = e.target.files[0];
+        setImage1(file);
+    };
+
+    const onSubmit = (data, event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        if (image) formData.append('image', image);
+
+        for (const key of Object.keys(product)) {
+            formData.set(key, product[key]);
+        }
+
+        if (isUpdate) {
+            fetch(
+                `https://dynobd-ecommerce.herokuapp.com/api/products//update-product`,
+                {
+                    method: 'PATCH',
+                    body: formData,
+                }
+            )
+                .then((response) => response.json())
+                .then((data) => {
+                    toast.success(data.message);
+                    console.log(data);
+                })
+                .catch((err) => {
+                    setErrorMsg(err.message);
+                    toast.error(data.error);
+                });
+        }
+    };
+
+    return (
+        <>
+            <ToastContainer />
+        </>
+    );
+};
+
+export default AddProduct;
